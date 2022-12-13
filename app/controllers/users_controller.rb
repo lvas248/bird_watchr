8aa58ef3dafe1ce4fe227dce46e93f73e38c394b
Luis <lvas248@gmail.com>
2022-12-13 17:22:28 -0500

    create user resource, add signup functionality to users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        session[:user_id] = user.id
+        render json: user, status: :ok
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
