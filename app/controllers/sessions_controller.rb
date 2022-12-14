cf3709587d38630348b8d3b37ea1a3d8bac324e5
Luis <lvas248@gmail.com>
2022-12-13 19:01:34 -0500

    create login component and build fetch for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
+      session[:user_id] = user.id
       render json: user, status: :ok
     else
       render json: { error: 'Invalid username or password'}, status: :unauthorized
@@ -14,7 +15,6 @@ class SessionsController < ApplicationController
     session.delete :user_id
     head :no_content
   end
-
 
 
 end
---
+++ client/src/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Form, Button, Input, Label } from 'reactstrap'
+
+function Login(){
+
+    const obj = { username: '', password: ''}
+
+    const [ loginObj, setLoginObj ] = useState(obj)
+
+    function updateLoginObj(key, value){
+        const copy = {...loginObj}
+        copy[key] = value
+        setLoginObj(copy)
+    }
+
+    function submitLogin(e){
+        e.preventDefault()
+        fetch('/login',{
+            method: 'POST',
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(loginObj)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        setLoginObj(obj)
+    }
+
+
+  
+    return (
+        <div id='form'>
+            <Form onSubmit={submitLogin}>
+                <Label>Username</Label>
+                <Input value={loginObj.username} onChange={e=> updateLoginObj('username', e.target.value)}/>
+
+                <Label>Password</Label>
+                <Input value={loginObj.password} onChange={e=> updateLoginObj('password', e.target.value)}/>
+
+               <Button color='primary'>Start</Button>
+            </Form>
+
+            <div id='toSignup'>
+                <p>New to BirdWatchr? <NavLink to='/signup'>Signup</NavLink></p>
+                
+            </div>
+
+        </div>
+    )
+}
+export default Login
